b694a667834e4a18a6d0d6f6093a328558bde0cb
Ibrohim <irgashev1200@gmail.com>
2021-03-26 21:07:47 +0500

    Finished 32streem all ok

--- frontend/api_product.php
+++ frontend/api_product.php
@@ -6,117 +6,116 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
 <body>
 <form class="delete-form">
     <div>
         <label class="col-sm-2 col-form-label">Идентификатор</label>
-        <input type="text" value="<?=$one['id'] ?? ''?>" name="id">
+        <input type="text" value="" name="id">
     </div>
     <div>
         <input type="submit">
     </div>
 </form>
-    <form class="form-horizontal create-form" action="/?model=product&action=save" method="post" enctype="multipart/form-data">
-        <div class="card-body">
-            <div>
-                <label class="col-sm-2 col-form-label">Идентификатор</label>
-                <input type="text" value="<?=$one['id'] ?? ''?>" name="id">
-            </div>
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Заголовок</label>
-                <div class="col-sm-10">
-                    <input type="text" value="<?=$one['title'] ?? ''?>" name="title" class="form-control">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label  class="col-sm-2 col-form-label">Фотография</label>
-                <div class="col-sm-10">
-                    <input type="file" value="<?=$one['picture'] ?? ''?>" name="picture" class="form-control">
-                </div>
-                <?php if (!empty($one['picture'])) { ?>
-                    <img src="http://localhost/shop/uploads/products/<?php print $one['picture'];?>" style="width: 70px;">
-                <?php } ?>
-            </div>
-            <div class="form-group row">
-                <label   class="col-sm-2 col-form-label">Пред-просмотр</label>
-                <div class="col-sm-10">
-                    <input type="text" value="<?=$one['preview'] ?? ''?>" name="preview" class="form-control">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label   class="col-sm-2 col-form-label">Содержание</label>
-                <div class="col-sm-10">
-                    <textarea rows="7" name="content" class="form-control"><?=$one['content'] ?? ''?></textarea>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label   class="col-sm-2 col-form-label">Цена</label>
-                <div class="col-sm-10">
-                    <input type="text" value="<?=$one['price'] ?? ''?>" name="price" class="form-control">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label   class="col-sm-2 col-form-label">Статус</label>
-                <div class="col-sm-10">
-                    <input type="text" value="<?=$one['status'] ?? ''?>" name="status" class="form-control">
-                </div>
-            </div>
-            <div>
-                <input type="submit" class="btn btn-info" value="Сохранить">
+<form class="form-horizontal create-form" action="/?model=product&action=save" method="post"
+      enctype="multipart/form-data">
+    <div class="card-body">
+        <div>
+            <label class="col-sm-2 col-form-label">Идентификатор</label>
+            <input type="text" value="" name="id">
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Заголовок</label>
+            <div class="col-sm-10">
+                <input type="text" value="" name="title" class="form-control">
             </div>
         </div>
-    </form>
-    <script>
-        $('form.delete-form').submit(function () {
-            let data = {
-                id: parseInt($('input[name=id]').val()),
-            };
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Фотография</label>
+            <div class="col-sm-10">
+                <input type="file" value="" name="picture" class="form-control">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Пред-просмотр</label>
+            <div class="col-sm-10">
+                <input type="text" value="" name="preview" class="form-control">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Содержание</label>
+            <div class="col-sm-10">
+                <textarea rows="7" name="content" class="form-control"></textarea>
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Цена</label>
+            <div class="col-sm-10">
+                <input type="text" value="" name="price" class="form-control">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2 col-form-label">Статус</label>
+            <div class="col-sm-10">
+                <input type="text" value="" name="status" class="form-control">
+            </div>
+        </div>
+        <div>
+            <input type="submit" class="btn btn-info" value="Сохранить">
+        </div>
+    </div>
+</form>
+<script>
+    $('form.delete-form').submit(function () {
+        let data = {
+            id: parseInt($('input[name=id]').val()),
+        };
 
-            $.ajax({
-                type: 'POST',
-                url: '/?module=api&model=product&action=delete',
-                data: data,
-                dataType: 'json',
-                success: function(response) {
-                    console.log(response);
-                },
-                error: function(data) {
-                    console.error(data)
-                }
-            });
-            return false;
-        })
+        $.ajax({
+            type: 'POST',
+            url: '/?module=api&model=product&action=delete',
+            data: data,
+            dataType: 'json',
+            success: function (response) {
+                console.log(response);
+            },
+            error: function (data) {
+                console.error(data)
+            }
+        });
+        return false;
+    })
 
 
-        $('form.create-form').submit(function () {
-            let data = {
-                id: parseInt($('input[name=id]').val()),
-                title: $('input[name=title]').val(),
-                preview: $('input[name=preview]').val(),
-                content: $('textarea[name=content]').val(),
-                picture: $('input[name=picture]').val(),
-                price: $('input[name=price]').val(),
-                status: $('input[name=status]').val(),
-            };
+    $('form.create-form').submit(function () {
+        let data = {
+            id: parseInt($('input[name=id]').val()),
+            title: $('input[name=title]').val(),
+            preview: $('input[name=preview]').val(),
+            content: $('textarea[name=content]').val(),
+            picture: $('input[name=picture]').val(),
+            price: $('input[name=price]').val(),
+            status: $('input[name=status]').val(),
+        };
 
-            const operation = data.id > 0 ? 'update' : 'create';
+        const operation = data.id > 0 ? 'update' : 'create';
 
-            $.ajax({
-                type: 'POST',
-                url: '/?module=api&model=product&action=' + operation,
-                data: data,
-                dataType: 'json',
-                success: function(response) {
-                    console.log(response);
-                },
-                error: function(data) {
-                    console.error(data)
-                }
-            });
-            return false;
-        })
-    </script>
+        $.ajax({
+            type: 'POST',
+            url: '/?module=api&model=product&action=' + operation,
+            data: data,
+            dataType: 'json',
+            success: function (response) {
+                console.log(response);
+            },
+            error: function (data) {
+                console.error(data)
+            }
+        });
+        return false;
+    })
+</script>
 </body>
 </html>
--- frontend/src/controller/api/ProductController.php
+++ frontend/src/controller/api/ProductController.php
@@ -5,22 +5,32 @@ include_once __DIR__ . "/../../../../common/src/service/UserService.php";
 
 class ProductController
 {
-	public function index()
-	{
-	    header("Charset: utf-8");
-	    header("Content-Type: application/json: charset=utf-8");
+    public function __construct()
+    {
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json: charset=utf-8");
+    }
 
-	    $all = (new Product())->getAllForExport(10);
+    public function index()
+    {
+        $all = (new Product())->getAllForExport(10);
 
-	    print json_encode($all);
-	    die();
-	}
+        print json_encode($all);
+        die();
+    }
 
-	public function create()
-	{
-	    header("Content-Type: application/json");
+    public function view()
+    {
 
-	    try {
+        $product = (new Product())->getById((int)$_GET['id']);
+
+        print json_encode($product);
+        die();
+    }
+
+    public function create()
+    {
+        try {
             $data = $_POST;
 
             $product = new Product(
@@ -40,18 +50,15 @@ class ProductController
             print json_encode([
                 'result' => 'OK'
             ]);
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             throw new Exception(json_encode(['result' => 'ERROR', 'message' => $e->getMessage()]), 400);
         }
-	    die();
-	}
+        die();
+    }
 
-	public function update()
-	{
-	    header("Content-Type: application/json");
-
-	    try {
+    public function update()
+    {
+        try {
             $data = $_POST;
 
             $product = new Product(
@@ -71,18 +78,15 @@ class ProductController
             print json_encode([
                 'result' => 'OK'
             ]);
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             throw new Exception(json_encode(['result' => 'ERROR', 'message' => $e->getMessage()]), 400);
         }
-	    die();
-	}
+        die();
+    }
 
-	public function delete()
-	{
-	    header("Content-Type: application/json");
-
-	    try {
+    public function delete()
+    {
+        try {
             $data = $_POST;
 
             if (!isset($data['id'])) {
@@ -94,10 +98,9 @@ class ProductController
             print json_encode([
                 'result' => 'OK'
             ]);
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             throw new Exception(json_encode(['result' => 'ERROR', 'message' => $e->getMessage()]), 400);
         }
-	    die();
-	}
+        die();
+    }
 }
